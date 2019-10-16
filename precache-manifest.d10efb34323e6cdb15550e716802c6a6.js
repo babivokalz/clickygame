b84c9da38c594ff4d0bda01cc25738f2e684f09b
AlaCrity <wlchen1105@gmail.com>
2019-10-16 23:00:53 +0200

    Updates

---
+++ precache-manifest.d10efb34323e6cdb15550e716802c6a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f1e9dabcd0deda1fde5826be274145",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "3aa950f7af21f1dbb8e3",
+    "url": "/clickygame/static/css/main.57ca4046.chunk.css"
+  },
+  {
+    "revision": "db0d34b1e6c97b0a94dc",
+    "url": "/clickygame/static/js/2.0e2be38d.chunk.js"
+  },
+  {
+    "revision": "3aa950f7af21f1dbb8e3",
+    "url": "/clickygame/static/js/main.603bba92.chunk.js"
+  },
+  {
+    "revision": "29a42c25b22d11848064",
+    "url": "/clickygame/static/js/runtime-main.158a7832.js"
+  }
+]);
